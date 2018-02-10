e9d8955ad75de3dbd95ce16f3e1e2efdabd35853
Madalinskim <36034346+Madalinskim@users.noreply.github.com>
2018-02-10 22:55:37 +0100

    Admin panel

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productID)
+        {
+            Product product = repository.Products.First(s => s.ProductID == productID);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("Zapisano {0} ", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        public ActionResult Delete(int productID)
+        {
+            Product deletedProduct = repository.DeleteProduct(productID);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = string.Format("Usunięto {0}", deletedProduct.Name);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
